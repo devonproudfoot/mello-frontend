f6ca7125debda3f6dc234c752a2d9cb34466f6d7
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-09-24 00:06:23 +0000

    Added class for js

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+class Card {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+class List {
+  constructor(title) {
+    this.title = title;
+    this.cards = [];
+  }
+
+  addCard(text) {
+    var card = new Card(text);
+    this.cards.push(card);
+  }
+}
+
+class Board {
+  constructor() {
+    this.lists = [];
+  }
+
+  addList(text) {
+    var list = new List(text);
+    this.lists.push(list);
+  }
+}
+
+var board = new Board();
+board.addList('New list');
+board.lists[0].addCard('New card');
+
+console.log(board);
